5a0cf28bbe9ac253fbb3186175ea8688590e8986
예지 <lee24pm@gmail.com>
2025-09-17 11:38:09 +0900

    최초 프로젝트 생성할 때 존재하는 파일

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
